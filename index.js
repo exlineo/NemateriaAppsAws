f033d9071e9053c45697dbd8110be75a15b05a37
exlineo <contact@exlineo.com>
2021-06-26 21:12:06 +0200

    Initialisation du projet

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Récupération des services AWS
+ */
+const { DynamoDBClient, ListTablesCommand } = require("@aws-sdk/client-dynamodb");
+const { S3Client, AbortMultipartUploadCommand } = require("@aws-sdk/client-s3");
+
+// Configuration
+const region = 'eu-west-3';
+
+(async () => {
+  const client = new DynamoDBClient({ region });
+  const command = new ListTablesCommand({});
+  try {
+    const results = await client.send(command);
+    console.log(results.TableNames.join("\n"));
+  } catch (err) {
+    console.error(err);
+  }
+})();
