c103098a86dd895f1d31d5bd386169505805fe01
exlineo <contact@exlineo.com>
2021-06-26 21:44:20 +0200

    Test d'information

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const { S3Client, AbortMultipartUploadCommand } = require("@aws-sdk/client-s3");
 // Configuration
 const region = 'eu-west-3';
 
-(async () => {
-  const client = new DynamoDBClient({ region });
-  const command = new ListTablesCommand({});
-  try {
-    const results = await client.send(command);
-    console.log(results.TableNames.join("\n"));
-  } catch (err) {
-    console.error(err);
-  }
-})();
+// (async () => {
+//   const client = new DynamoDBClient({ region });
+//   const command = new ListTablesCommand({});
+//   try {
+//     const results = await client.send(command);
+//     console.log(results.TableNames.join("\n"));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// })();
+
+exports.handler = async (event) => {
+  return s3.listBuckets().promise();
+}
